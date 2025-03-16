aed9b3bf4c4049988ac704ef5cf0dbb0e3a26b27
Fabian Melo <fabianmelov@gmail.com>
2025-03-16 00:58:59 -0500

    Se ingresan valores para build APK

--- lib/services/export_service.dart
+++ lib/services/export_service.dart
@@ -4,7 +4,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:csv/csv.dart';
 import 'dart:convert';
-import '../models/report.dart';
 import 'database_helper.dart';
 
 class ExportService {
